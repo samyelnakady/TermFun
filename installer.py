43d82e21b507ad9ffef45e579d5d9ab62a8f39cf
f00kthisc0de <76871383+f00kthisc0de@users.noreply.github.com>
2021-02-13 14:00:35 +0100

    Add files via upload

--- installer.py
+++ installer.py
@@ -4,6 +4,7 @@ from os import system as command
 
 print("Installing everything needet")
 command("pkg install python; pkg install python3; pip install termcolor; pkg install termux-api; pkg install curl;cp TermFun /data/data/com.termux/files/usr/bin && cp TermFunListener /data/data/com.termux/files/usr/bin ;cd /sdcard; curl https://f-droid.org/repo/com.termux.api_47.apk -O; pm install com.termux.api_47.apk; pkg install nmap;")
+command("chmod +x /data/data/com.termux/files/usr/bin/TermFun; chmod +x /data/data/com.termux/files/usr/bin/TermFunListener")
 command("clear")
 print("The Termux-API apk  was placed on your /sdcard install it please.")
 print("You can also download the Termux-API from the appstore if you want.")
---
+++ uninstall.py
@@ -0,0 +1,5 @@
+#!/data/data/com.termux/files/usr/bin/python3
+
+from os import system as command
+
+command("cd ..; rm -rf TermFun; rm /data/data/com.termux/files/usr/bin/TermFun; rm /data/data/com.termux/files/usr/bin/TermFunListener")
